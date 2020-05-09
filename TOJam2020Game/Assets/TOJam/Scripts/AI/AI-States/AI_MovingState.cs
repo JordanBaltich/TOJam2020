db9faff954cea822e333a19f01de22c182191294
Jordan <jordan.baltich@gmail.com>
2020-05-09 15:46:37 -0400

    Some AI fixes

--- TOJam2020Game/Assets/TOJam/Scripts/AI/AI-States/AI_MovingState.cs
+++ TOJam2020Game/Assets/TOJam/Scripts/AI/AI-States/AI_MovingState.cs
@@ -25,11 +25,15 @@ public class AI_MovingState : StateMachineBehaviour
             m_Controller.m_Agent.SetDestination(m_Vision.CheckForPlayerUnits());
         }
 
-        if (m_Vision.currentTarget.gameObject.tag == m_Vision.aIControlTag)
+        if (m_Vision.currentTarget != null)
         {
-            m_Controller.m_Agent.destination = animator.transform.position;
-            animator.SetBool("TargetFound?", false);
+            if (m_Vision.currentTarget.tag == m_Vision.aIControlTag)
+            {
+                m_Controller.m_Agent.destination = animator.transform.position;
+                animator.SetBool("TargetFound?", false);
+            }
         }
+       
 
         if (distanceToTarget <= m_Controller.m_Data.attackRange)
         {
